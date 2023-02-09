b231fdb522dbe5408ae175ac8d26e65bd6c72307
Peter Woolsey <woolsey@student.ubc.ca>
2023-02-09 00:26:07 -0800

    first attempt, doesn't really work, gonna try Go

--- src/main.cr
+++ src/main.cr
@@ -1,6 +1,7 @@
 PORT = 8080
 require "http/server"
 require "json"
+
 module Main
   VERSION = "0.1.0"
 
@@ -25,7 +26,7 @@ module Main
   end
 
   class Dictionary
-    def initialize()
+    def initialize
       @dictionary = Hash(Int32, Entry).new
     end
 
@@ -53,22 +54,22 @@ module Main
     def description
       return @description
     end
-    
+
     def time
       return @time
     end
-    
+
     def urgency
       return @urgency
     end
-    
+
     def area
       return @area
     end
   end
 
   class Data
-    def initialize()
+    def initialize
       @errors = Hash(Int32, Error).new
     end
 
@@ -77,32 +78,31 @@ module Main
     end
 
     def remove(number : Int32)
-      if @errors[number].nil?
+      if @errors[number].nil? #known issue -> can't recognize when error is real but not presently active?
         return nil
       end
       @errors.delete(number)
       return number
     end
 
-    def make_json()
+    def make_json
       return JSON.build do |json|
-        #json.field("errors") do
-          json.object do
-            @errors.each do |number, error|
-              json.field(number.to_s) do
-                json.object do
-                  json.field("description", error.description)
-                  json.field("time", error.time)
-                  json.field("urgency", error.urgency)
-                  json.field("area", error.area)
-                end
+        # json.field("errors") do
+        json.object do
+          @errors.each do |number, error|
+            json.field(number.to_s) do
+              json.object do
+                json.field("description", error.description)
+                json.field("time", error.time)
+                json.field("urgency", error.urgency)
+                json.field("area", error.area)
               end
             end
+          end
         end
       end
     end
   end
-
 
   error_dictionary = Dictionary.new
   # set up error 1
@@ -112,35 +112,25 @@ module Main
   error_dictionary.add_word(2, error_2)
   error_3 = Entry.new(description: "overvoltage", urgency: 0, area: "BMS")
   error_dictionary.add_word(3, error_3)
-    
+
   data = Data.new
-  #data.add_error(123, Error.new(description: "overcurrent", time: 12134, urgency: 0, area: "BMS"))
-  #data.add_error(1, Error.new(description: "overcurrent", time: 12134, urgency: 0, area: "BMS"))
-  #data.add_error(23, Error.new(description: "overcurrent", time: 12134, urgency: 0, area: "BMS"))
-  #data.remove(1)
-
-
   server = HTTP::Server.new do |context|
-    
     current_path = context.request.path.lchop
-    command = current_path.split("/").first #this will need to be fixed when adding the set command
-    case(command)
-
+    command = current_path.split("/").first # this will need to be fixed when adding the set command
+    case (command)
     when "version"
       context.response.content_type = "text/plain"
       context.response.print VERSION
-
     when "get"
       context.response.content_type = "application/json"
       context.response.print data.make_json
-
     when "add"
       input_code = current_path.split("/").last.to_i
       error = error_dictionary.get_entry(input_code)
       if error.nil?
         context.response.content_type = "text/plain"
         context.response.print "Invalid"
-      else 
+      else
         data.add_error(input_code, Error.new(description: error.description, time: Time.utc.to_unix, urgency: error.urgency, area: error.area))
         context.response.content_type = "text/plain"
         context.response.print "#{input_code}"
@@ -158,8 +148,6 @@ module Main
     else
       context.response.print "Unexpected command: #{command}"
     end
-
-
   end
 
   address = server.bind_tcp PORT
